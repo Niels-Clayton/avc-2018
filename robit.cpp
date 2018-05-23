78e8108f81556ac09b707114b2c77b59c35906d2
DeanRae <aladeanra@gmail.com>
2018-05-23 15:08:50 +1200

    Add new robit code that does quadrant 1 and 2

--- robit.cpp
+++ robit.cpp
@@ -3,23 +3,22 @@
 #include "E101.h"
 #include <sys/time.h>
 
-/** Opens the gate **/
-void doGate(){
-   //connects to server with the ip address 
-   connect_to_server("130.195.6.196", 1024);
-   char message[24] = {"Please"};
-   send_to_server(message);
-   receive_from_server(message); //this may be buggy!
-   send_to_server(message);
-	}
-
-
-int move_forward(int speed){
-	set_motor (1, speed);
-	set_motor (2, speed);
-	sleep1(0, 250000);
-	set_motor(1, 0);
-	set_motor(2, 0);
+/*
+ * Function: do_gate
+ * ----------------------------
+ *   Returns the colour threshold i.e. when a pixel colour is considered black
+ *   (less than threshold) or white (more than threshold)
+ *
+ *   returns: int threshold
+ *
+ */
+void do_Gate()
+{
+    connect_to_server("130.195.6.196", 1024);
+    char message[24] = {"Please"};
+    send_to_server(message);
+    receive_from_server(message); 
+    send_to_server(message);
 }
 
 /*
@@ -59,7 +58,7 @@ int get_color_threshold()
 		/* image is likely to be all black so set thr to 255 */
 		threshold = 255;
 	}
-	printf("%d \n %d" , max, min);
+	//printf("%d \n %d" , max, min);
 
 	return threshold;
 }
@@ -78,7 +77,6 @@ int get_color_threshold()
  */
 void get_white_pixels(int threshold, int pixels[])
 {
-	
 	for(int i = 0; i < 320; i++)
 	{ 
 		pixels[i] = 0; // pixel is black
@@ -89,6 +87,27 @@ void get_white_pixels(int threshold, int pixels[])
  			 pixels[i] = 1; // pixel is white
 		}
 	}
+}
+
+/*
+ * Function: red_pixels
+ * ----------------------------
+ *   Checks for the presence of red pixels
+ *
+ *   returns: int red - the number of red pixels
+ *
+ */
+int red_pixels()
+{
+	int red = 0;
+	for(int i = 0; i < 320; i++)
+	{
+		if(get_pixel(120, i, 0)>200 && get_pixel(120, i, 1)<100 && get_pixel(120, i, 2)<100)
+		{
+			red ++;
+		}
+	}
+	return red;
 }
 
 /*
@@ -105,7 +124,7 @@ int calculate_error()
 	int wp = 0; // number of white pixels
 	int pixels[320];
 
-	get_white_pixels(get_color_threshold(),pixels);
+	get_white_pixels(get_color_threshold(),pixels); //initialise array
 
 	for(int i = 0; i < 320; i++)
 	{ 
@@ -116,13 +135,13 @@ int calculate_error()
 		}
 	}
 
-	if(wp >= 310){
+	if(wp == 320){
 		return 20000; // case for if sensor only senses white
 	}else if(wp == 0){
 		return 10000; // case for if sensor only senses black
-	}else if((wp > 140 && wp < 180) && pixels[0] == 1 && pixels[10] == 1 && pixels[20] == 1){
+	}else if((wp > 100 && wp < 180) && error < 0){
 		return -30000; // case for if half the screen has white 
-	}else if ((wp > 140 && wp < 180) && pixels[320] == 1 && pixels[310] == 1 && pixels[300] == 1){
+	}else if ((wp > 100 && wp < 180) && error > 0){
 		return 30000;
 	}else{
 		return error/wp;
@@ -137,207 +156,122 @@ int calculate_error()
  *   returns: int final error
  *
  */
-int calculate_pid()
+int calculate_pid(int currentError, int previousError, long elapsed)
 {
-	/* 
-	 * NOTE: move previousError and takePic to main function once code for motor 
-	 *       control has been made 
-	 */
-	int currentError = calculate_error();
-	if(currentError == 10000)
-	{
-		/* 
-		 * do something (go back?) - 10000 is an error signal to indicate that 
-		 * the robot does not detect a line (i.e. all background)
-		 */
-	}
-	int previousError = 1;
-	double kp = 0.005;
-	double kd = 0.005;
+	
+	/* Set kp and kd */
+	double kp = 0.95;
+	double kd = -0.40;
 	double proportionalSignal = 0.0;
 	double derivativeSignal = 0.0;
 
+	/* Calculate Signals */
 	proportionalSignal = (double)currentError * kp;
-	derivativeSignal = ((double)currentError - (double)previousError) * kd;
-	double finalSignal = proportionalSignal + derivativeSignal;
+	
+	derivativeSignal = (((double)currentError - (double)previousError) * kd)/elapsed;
 
-	previousError = currentError;
-
-	return finalSignal;
+	return proportionalSignal + derivativeSignal; // return final signal
 
 }
 
+void set_motor_speed(int motor1Speed, int motor2Speed)
+{
+	set_motor(1, motor1Speed);
+	set_motor(2, motor2Speed);
+}
 
-
-int main(){
-	init();
-	int sector = 0;
-
-	while(true){
-
+void quadrant2()
+{
+	while(true)
+	{
+	
+		/* Calculate Time */
 		struct timeval t1;
 		struct timeval t2;
+		long elapsed = 1;
+		gettimeofday(&t1,0);
+
+		/* Take Images */
+		open_screen_stream();
+		take_picture();
+		update_screen();
 		
-		if(sector == 0){
-			doGate();
-			//Whatever makes it go forward
-			sector ++;
-		}
-		else if(sector == 1){
-			struct timeval t1;
-			struct timeval t2;
-			long elapsed = 1;
-			open_screen_stream();
-			take_picture();
-			update_screen();
-			//int previousError = 1;
+		//if no red pixels have been detect, do normal motor controls
+		if(red_pixels() <50)
+		{
+			/* Motor Speed Calculations */
 			int currentError = calculate_error();
 			int previousError = 1;
-			gettimeofday(&t1,0);
-			
-			if(currentError == 10000)
+
+			if(currentError == 10000) 
 			{
-				set_motor(1,-120);
-				set_motor(2,-120);
-				sleep1(0,200000);
-				if(currentError == 10000 && previousError == 10000){
-				set_motor(1, -100);
-				set_motor(2, -100);
-				}
-			 }else if(currentError == 20000){
-				 //sector++;
-				 set_motor(1,0);
-			     set_motor(2,0);
-				 sleep1(2,0);
-			 }
-
-			double kp = 0.95;
-			double kd = 0.40;
-			double proportionalSignal = 0.0;
-			double derivativeSignal = 0.0;
-
-			proportionalSignal = (double)currentError * kp;
-
-			derivativeSignal = (((double)currentError - (double)previousError) * kd)/elapsed;
-			
-			double finalSignal = proportionalSignal - derivativeSignal;
-
-			previousError = currentError;
-			
-			t2 = t1;
-			elapsed = (t2.tv_sec - t1.tv_sec)*1000000 +(t2.tv_usec-t1.tv_usec);
-			
-			printf("current error: %d\n", currentError);
-			printf("previous error: %d\n", previousError);
-			printf("pid: %f\n", finalSignal);
-			printf("leftMot: %d\n", (int)(120 + finalSignal));
-			printf("rightMot: %d\n",(int) (120 - finalSignal));
-			//return finalSignal;
-			if(finalSignal != 10000 || finalSignal !=20000 || finalSignal !=30000){
-				set_motor(1, 120 + finalSignal);
-				set_motor(2, 120 - finalSignal);
-			}else{
-				printf("Going backward\n");
-				set_motor(1, 135);
-				set_motor(2, 135);
-				sleep1(0,200000);
-				set_motor(1, 0);
-				set_motor(2, 0);
-				
-			}
-			
-		}
-		else if(sector == 2){
-			struct timeval t1;
-			struct timeval t2;
-			long elapsed = 1;
-			/** Sector 1 Code Goes in here - Squiggly Line **/
-			open_screen_stream();
-			take_picture();
-			update_screen();
-			
-			int currentError = calculate_error();
-	
-			int previousError = 1;
-			gettimeofday(&t1,0);
-			
-			if(currentError == 10000)
-			{
-				set_motor(1,-120);
-				set_motor(2,-120);
+				// If image is all black go backwards
+				set_motor_speed(-120,-120);
 				sleep1(0,250000);
-				if(currentError == 10000 && previousError == 10000){
-				set_motor(1, -80);
-				set_motor(2, -80);
+				// If image is still black in next image, slowly go backwards again
+				if(currentError == 10000 && previousError == 10000)
+				{
+					set_motor_speed(-80, -80);
 				}
-			}else if(currentError == -30000){
-				set_motor(1, 120);
-				set_motor(2, 120);
-				sleep1(1,500000);
-				set_motor(1, 120);
-				set_motor(2, 0);
-				sleep1(1,0 );
 			}
-			else if(currentError == 30000){
-				set_motor(1, 120);
-				set_motor(2, 120);
-				sleep1(1,500000);
-				set_motor(1, 0);
-				set_motor(2, 120);
-				sleep1(1,0 );
-			}
-			else if(currentError == 20000){
-				set_motor(1, 0);
-				set_motor(2, 0);
-				sleep1(5,0);
-			}
+			//else if(currentError == -30000)
+			//{
+				// If half of the image has white only on the left side
+			//	set_motor_speed(100,100);
+			//	sleep1(0, 500000);
+			//	set_motor_speed(0, 100);
+			//	sleep1(0,500000 );
+			//}
+			//else if(currentError == 30000 || currentError == 20000)
+			//{
+			//	// If half of the image has white only on the right side or straight line
+			//	set_motor_speed(100,100);
+			//	sleep1(0, 500000);
+			//	set_motor_speed(100, 0);
+			//	sleep1(0,500000 );
+			//}
 			
-			
-			double kp = 0.90;
-			double kd = -0.25;
-			double proportionalSignal = 0.0;
-			double derivativeSignal = 0.0;
 
-			proportionalSignal = (double)currentError * kp;
-			
-			derivativeSignal = (((double)currentError - (double)previousError) * kd)/elapsed;
-			
-			double finalSignal = proportionalSignal + derivativeSignal;
-
-			previousError = currentError;
-		
+			int finalSignal = calculate_pid(currentError, previousError, elapsed);
+			previousError = currentError;		
 			t2 = t1;
 			elapsed = (t2.tv_sec - t1.tv_sec)*1000000 +(t2.tv_usec-t1.tv_usec);
+
+			if(currentError != 10000)
+			{
+				set_motor_speed(120 + finalSignal, 120 - finalSignal);
 			
-			printf("current error: %d\n", currentError);
-			printf("previous error: %d\n", previousError);
-			printf("pid: %f\n", finalSignal);
-			printf("leftMot: %d\n", (int)(120 + finalSignal));
-			printf("rightMot: %d\n",(int) (120 - finalSignal));
-			//return finalSignal;
-			if(finalSignal != 10000){
-				set_motor(1, 100 + finalSignal);
-				set_motor(2, 100 - finalSignal);
-			}else{
-				printf("Going backward\n");
-				set_motor(1, 135);
-				set_motor(2, 135);
-				sleep1(0,200000);
-				set_motor(1, 0);
-				set_motor(2, 0);
-				
 			}
+
+			sleep1(0, 5000000);
 		}
-		else if(sector == 3){
-			/** Sector 3 Code goes in here - Maze Naviagaton **/
+		else
+		{
+			break;
 		}
 	}
-	/*
-	//Reset motors to 0
-	set_motor(1, 0);
-	set_motor(2, 0);
-	sleep1(5, 000000);
-	//int speed = 40;
-	move_forward(speed);
-	* */
-return 0;}
+}
+
+int main()
+{
+	init();
+	int quadrant = 1; 	
+	while(true){
+		switch(quadrant)
+		{
+			case 1 :
+				do_Gate();
+				quadrant = 2;
+				break;
+			case 2 :
+				quadrant2();
+				quadrant = 3;
+				break;
+			default:
+				set_motor_speed(0,0);
+		}
+	}
+	return 0;
+
+}
+
